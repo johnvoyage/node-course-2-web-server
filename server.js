4b24a25a3f9ea3374d59f17081b0ff2af96a3523
johnvoyage <jmyork762@gmail.com>
2018-07-10 14:37:39 -0400

    Minor project page text fix

--- server.js
+++ server.js
@@ -52,7 +52,8 @@ app.get("/about", (req, res) => {
 
 app.get("/projects", (req, res) => {
   res.render("projects.hbs", {
-    pageTitle: "Project Page"
+    pageTitle: "Project Page",
+    projectMessage: "Welcome to my projects page"
   });
 });
 
